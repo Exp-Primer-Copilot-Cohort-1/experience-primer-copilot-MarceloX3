7232bb80fdf85a8ba8498ee84ada01eefdcd718a
Marcelo Alejandro Ortiz Avila <124118183+MarceloX3@users.noreply.github.com>
2024-06-11 23:00:54 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,24 @@
+function skillsMember() {
+  return [
+    {
+      skill: 'JavaScript',
+      level: 'Intermediate',
+    },
+    {
+      skill: 'HTML',
+      level: 'Intermediate',
+    },
+    {
+      skill: 'CSS',
+      level: 'Intermediate',
+    },
+    {
+      skill: 'React',
+      level: 'Intermediate',
+    },
+    {
+      skill: 'NodeJS',
+      level: 'Intermediate',
+    },
+  ];
+}
